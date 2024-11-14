f7013cb9cee38dc17f3a3ac6fc1cd3958e719fa2
alifmaulidanar <alifmaulidanr@gmail.com>
2024-11-14 08:33:13 +0700

    Prepare admin auth page & dashboard

---
+++ src/app/(admin)/dashboard/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import FormSignIn from "./_components/form"
+
+export const description =
+  "A simple login form with email and password. The submit button says 'Sign in'."
+
+export default function SignInPage() {
+  return (
+    <main className="w-full h-screen overflow-hidden relative">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <FormSignIn />
+      </div>
+    </main>
+  )
+}

b6931d444db803e68ecd0a3c15661aa0956125e0
alifmaulidanar <alifmaulidanr@gmail.com>
2024-11-14 08:35:05 +0700

    Added customer signin, signup, and dashboard pages

---
+++ src/app/(customer)/(index)/layout.tsx
@@ -0,0 +1,19 @@
+import "../../globalsLanding.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ['300', '400', '500', '700', '800'],
+  subsets: ['latin-ext'],
+});
+
+export default function LandingRootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
